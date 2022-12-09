09291be346494476c7a22078fa2103b7da872145
Sejin Oh <soh52@my.bcit.ca>
2022-12-08 16:15:04 -0800

    font changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Nav from "../components/Nav";
 function Home() {
   return (
     // <div className="flex justify-center items-center flex-col ">
-      <div class="flex flex-col items-center justify-center bg-[#D6EADF] h-screen w-screen font-comfortaa px-8">
+      <div class="flex flex-col items-center justify-center bg-[#D6EADF] h-screen w-screen px-8">
         <img
           src="../SmallLogo.png"
           alt="small logo"
@@ -16,17 +16,17 @@ function Home() {
         <h1 class="flex text-[#5F7BB1] text-4xl text-center my-8 headingFont">
           Personal Woosh
         </h1>
-        <p class="flex justify-center text-[#5F7BB1] text-lg text-center">
+        <p class="flex justify-center text-[#5F7BB1] text-lg text-center bodyFont">
           Woosh is an app designed to teach people about basic personal hygiene.
         </p>
-        <p class="flex justfy-center text-[#5F7BB1] text-lg text-center mb-16">
+        <p class="flex justfy-center text-[#5F7BB1] text-lg text-center mb-16 bodyFont">
           You may take the quiz and see where your knowledge is on this topic.
         </p>
-        <A href="/quizcategory">
+        <A href="/quizcategory" >
           <BigButton />
         </A>
         <A href="/instructions">
-          <p class="text-[#5F7BB1] mt-6 underline text-lg">
+          <p class="text-[#5F7BB1] mt-6 underline text-lg bodyFont">
             or watch the instruction
           </p>
         </A>
